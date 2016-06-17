ded80e07cd90278ba3501201632faa02de886157
Amit Saha <amitsaha.in@gmail.com>
2016-06-17 14:03:41 +1000

    Obfuscation service

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
-FROM golang:alpine
-RUN mkdir /src
-ADD application.go /src/application.go
+FROM golang
+ADD application.go /go/src/github.com/amitsaha/linux_voice_5/application.go
+RUN cd /go/src/github.com/amitsaha/linux_voice_5/ && go get -d -v .
 EXPOSE 5000
-CMD ["go", "run", "/src/application.go"]
-
-
-
+CMD ["go", "run", "/go/src/github.com/amitsaha/linux_voice_5/application.go"]
--- application.go
+++ application.go
@@ -1,15 +1,41 @@
-// Basic HTTP server listening on 5000
+// Basic Integer Obfuscation service using hashids
+// (http://hashids.org/go/)
+
+// Usage:
+// Start server: go run application.go
+// Client: curl 127.0.0.1:5000?id=2
+// Vj7ygQ
+
 package main
 
 import (
 	"fmt"
+	"github.com/speps/go-hashids"
+	"math/rand"
 	"net/http"
-	"runtime"
+	"strconv"
 )
 
+func getHashId(id int) (string, error) {
+	hd := hashids.NewData()
+	hd.Salt = strconv.Itoa(rand.Int())
+	hd.MinLength = 6
+	h := hashids.NewWithData(hd)
+	return h.Encode([]int{id})
+}
+
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World. Running: ")
-	fmt.Fprintf(w, runtime.Version())
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.Error(w, "Bad id supplied", 400)
+	} else {
+		generatedId, err := getHashId(id)
+		if err != nil {
+			http.Error(w, "Error generating Id", 500)
+		} else {
+			fmt.Fprintf(w, generatedId)
+		}
+	}
 }
 
 func main() {
